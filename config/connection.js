aa498439bd8e52102c872c69dd14b11935755970
Lucas Mateus <lucasmateus290903@gmail.com>
2024-10-01 02:14:58 -0300

    .

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  "postgres://extraordinariosaber_user:L8JNYnAF59mhp4zitJqkGsSBZAZ1GVlL@dpg-crtmd09u0jms73ah6c50-a:5432/extraordinariosaber"
+  "postgresql://extraordinariosaber_user:L8JNYnAF59mhp4zitJqkGsSBZAZ1GVlL@dpg-crtmd09u0jms73ah6c50-a/extraordinariosaber"
 );
 
 (async () => {
