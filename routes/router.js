0d18e2982796eb3021668f792d64747959203154
Lucas Mateus <lucasmateus290903@gmail.com>
2024-10-01 00:24:46 -0300

    Mudança nos routes

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,5 @@
 import { authRouter } from "./auth_routes.js";
 import { categoriaRouter } from "./categoria_routes.js";
-import { pedidoRouter } from "./pedido_routes.js";
-import { produtoRouter } from "./produto_routes.js";
 import express from "express";
 import verifyJWT from "../middleware/auth_middleware.js";
 
